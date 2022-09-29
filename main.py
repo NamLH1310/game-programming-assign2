aa0a9dd798aa00df6de317551e162f368de48a15
Lê Hoàng Nam <namlehoang15@gmail.com>
2022-09-29 22:34:33 +0700

    Add left right movement

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from enum import Enum
 
 import pygame as pg
 import logging as log
@@ -20,6 +21,15 @@ KEN_STAGE_PATHS = [
 ]
 
 RYU_SPRITES_PATH = 'assets/Ryu.png'
+
+
+class State(Enum):
+    MOVE_RIGHT = 'move_right'
+    MOVE_LEFT = 'move_left'
+    IDLE = 'idle'
+    ATTACK = 'attack'
+    GUARD = 'guard'
+    JUMP = 'jump'
 
 
 class SpriteSheet(ABC):
@@ -47,41 +57,94 @@ class BackgroundSprite(SpriteSheet):
 
 
 class Player(SpriteSheet):
-    def __init__(self, path: str, flip=False):
+    def __init__(self, path: str, x: int, y: int, flip=False):
         super().__init__()
         scaler = 2.5
         self.sprite = pg.image.load(path).convert()
+        self.state = State.IDLE
+        self.velocity = 4
         self.flip = flip
-        self.sprites: [Surface] = [
-            self.sprite.subsurface(pg.Rect(0, 0, 70, 120)),
-            self.sprite.subsurface(pg.Rect(70, 0, 70, 120)),
-            self.sprite.subsurface(pg.Rect(140, 0, 70, 120)),
-            self.sprite.subsurface(pg.Rect(205, 0, 70, 120)),
-            self.sprite.subsurface(pg.Rect(270, 0, 70, 120)),
+        self.x = x
+        self.y = y
+
+        self.idle_sprites: list[Surface] = [
+            self.sprite.subsurface(pg.Rect(0, 0, 70, 110)),
+            self.sprite.subsurface(pg.Rect(70, 0, 70, 110)),
+            self.sprite.subsurface(pg.Rect(140, 0, 70, 110)),
+            self.sprite.subsurface(pg.Rect(205, 0, 70, 110)),
+            self.sprite.subsurface(pg.Rect(270, 0, 70, 110)),
         ]
-        for sprite in self.sprites:
+        self.move_right_sprites: list[Surface] = [
+            self.sprite.subsurface(pg.Rect(70, 120, 70, 110)),
+            self.sprite.subsurface(pg.Rect(145, 120, 70, 110)),
+            self.sprite.subsurface(pg.Rect(220, 120, 70, 110)),
+        ]
+        self.move_left_sprites: list[Surface] = [
+            self.sprite.subsurface(pg.Rect(295, 120, 70, 110)),
+            self.sprite.subsurface(pg.Rect(365, 120, 65, 110)),
+            self.sprite.subsurface(pg.Rect(425, 120, 65, 110)),
+            self.sprite.subsurface(pg.Rect(490, 120, 65, 110)),
+        ]
+        for sprite in self.idle_sprites + self.move_right_sprites + self.move_left_sprites:
             sprite.set_colorkey(BLUE, pg.RLEACCEL)
 
-        self.sprites = [
+        self.idle_sprites = self.scale_sprite(self.idle_sprites, scaler)
+        self.move_right_sprites = self.scale_sprite(self.move_right_sprites, scaler)
+        self.move_left_sprites = self.scale_sprite(self.move_left_sprites, scaler)
+        self.current_sprites = self.idle_sprites
+
+        self.y = y - self.idle_sprites[0].get_height()
+        self.x = x
+        if self.flip:
+            self.x -= self.idle_sprites[0].get_width()
+        self.w = self.current_sprites[self.index].get_width()
+        self.h = self.current_sprites[self.index].get_height()
+
+    @staticmethod
+    def scale_sprite(sprites: list[Surface], scaler: float) -> list[Surface]:
+        return [
             pg.transform.scale(sprite, (sprite.get_width() * scaler, sprite.get_height() * scaler))
-            for sprite in self.sprites
+            for sprite in sprites
         ]
 
     def get_sprite(self) -> Surface:
+        match self.state:
+            case State.IDLE:
+                self.current_sprites = self.idle_sprites
+            case State.MOVE_RIGHT:
+                self.current_sprites = self.move_right_sprites
+            case State.MOVE_LEFT:
+                self.current_sprites = self.move_left_sprites
+
+        if self.index >= len(self.current_sprites):
+            self.index = 0
+
         self.current_num_frames += 1
         if self.current_num_frames >= self.max_num_frames:
             self.current_num_frames = 0
-            self.index = (self.index + 1) % len(self.sprites)
+            self.index = (self.index + 1) % len(self.current_sprites)
 
         if self.flip:
-            return pg.transform.flip(self.sprites[self.index], True, False)
+            return pg.transform.flip(self.current_sprites[self.index], True, False)
+        return self.current_sprites[self.index]
 
-        return self.sprites[self.index]
+    def get_coord(self) -> tuple[int, int]:
+        return self.x, self.y
 
-    def get_init_coord(self, x: int, y: int) -> tuple[int, int]:
-        if self.flip:
-            return x - self.sprites[0].get_width(), y - self.sprites[0].get_height()
-        return x, y - self.sprites[0].get_height()
+    def handle_input(self):
+        key_pressed = pg.key.get_pressed()
+        if key_pressed[pg.K_RIGHT]:
+            self.state = State.MOVE_RIGHT
+            self.x += self.velocity
+            if self.x > 1280 - self.w:
+                self.x = 1280 - self.w
+        elif key_pressed[pg.K_LEFT]:
+            self.state = State.MOVE_LEFT
+            self.x -= self.velocity
+            if self.x < 0:
+                self.x = 0
+        else:
+            self.state = State.IDLE
 
 
 class GameManager:
@@ -96,13 +159,18 @@ class GameManager:
         self.timer = 90.0
         self.delta_t = 1 / self.fps
         self.bg_sprite: SpriteSheet = BackgroundSprite(KEN_STAGE_PATHS)
-        self.player1 = Player(RYU_SPRITES_PATH)
-        self.player2 = Player(RYU_SPRITES_PATH, True)
+        self.player_idx = 0
+        self.players: list[Player] = [
+            Player(RYU_SPRITES_PATH, 50, 620),
+            Player(RYU_SPRITES_PATH, 1230, 620, True),
+        ]
 
     def run(self):
         # main loop
         while not self.game_over:
             # get single input
+            self.players[self.player_idx].handle_input()
+
             for event in pg.event.get():
                 match event.type:
                     case pg.QUIT:
@@ -123,12 +191,12 @@ class GameManager:
             (0, 0),
         )
         self.screen.blit(
-            self.player1.get_sprite(),
-            self.player1.get_init_coord(50, 630),
+            self.players[self.player_idx].get_sprite(),
+            self.players[self.player_idx].get_coord(),
         )
         self.screen.blit(
-            self.player2.get_sprite(),
-            self.player2.get_init_coord(1230, 630),
+            self.players[1 - self.player_idx].get_sprite(),
+            self.players[1 - self.player_idx].get_coord(),
         )
         pg.display.update()
 
